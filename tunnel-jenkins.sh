899374ad66b175797cbb292f4bea8e8bd583258d
mytotoe <mytotoe@gmail.com>
2022-06-01 15:27:46 +0700

    Update tunnel-jenkins.sh

--- tunnel-jenkins.sh
+++ tunnel-jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 gcloud compute ssh jenkins \
-    --project devops-malaysia-jenkins-gke \
+    --project smart-citizen-digital-platform \
     --zone us-central1-a \
     -- -L 8080:localhost:8080
